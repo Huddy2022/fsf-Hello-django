be7f66a688e2ad2e637f09a698bd2c5c2e2fd0ba
Huddy2022 <mlvnhud@aol.com>
2023-02-28 20:11:34 +0000

    set up enviroment varibales and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["huddytest-rb-django-todo-app.herokuapp.com"]
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
 
 # Application definition
